54466ebb699d0ba489ae238d230bf64e693595c5
easyscripter <deniscs49@gmail.com>
2021-01-26 16:46:54 +0300

    add routes with params

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,22 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "@/views/Home";
+import ProfilePage from "@/views/ProfilePage";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/profile=:profileId',
+    name: 'ProfilePage',
+    component: ProfilePage
+  }
+];
 
 const router = new VueRouter({
   routes
